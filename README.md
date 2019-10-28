f90cb6cd520a8fcb0f3bf809044683ea90d18d89
giorgiobraz <giorgiobraz@gmail.com>
2019-10-28 17:02:26 -0300

    modelagem de entidades e relacionamentos

--- README.md
+++ README.md
@@ -1,3 +1,33 @@
 # Projeto BD1
 
-Projeto final para a disciplina de Banco de Dados 1
+Implementação de um sistema de papelaria para o projeto final da disciplina de Banco de Dados 1 do curso Bacharelado em Ciência da Computação em 2019/2 (UTFPR-CM).
+
+## Conteúdo do projeto
+- Modelo relacional utilizando o MySQL Workbench
+- Frameworks: Flask e Bootstrap 4
+
+## Criação do ambiente virtual com Virtualenv
+
+### Instalação:
+`sudo pip install virtualenv`
+
+### Inicializando um ambiente virtual:
+`mkdir <nome_do_diretório> && cd <nome_do_diretório>/`
+`virtualenv -p python3 .`
+
+### Script de Ativação do ambiente virtual:
+`source bin/activate`
+
+### Criação de arquivo com as dependências do sistema
+`pip freeze >> requirements.txt`
+
+### Instalação das dependências com o ambiente virtual ativo
+`pip install -r requiremennts.txt`
+
+### Como Desativar seu ambiente virtual:
+`deactivate`
+
+---
+### Referências
+- [Como Programar em Ambientes Virtuais](https://pythonacademy.com.br/blog/python-e-virtualenv-como-programar-em-ambientes-virtuais)
+- [Tutorial do prof. Rodrigo Hubner](https://github.com/rodrigohubner/PSB)
--- papelaria.sql
+++ papelaria.sql
@@ -1,60 +1,31 @@
-CREATE DATABASE PAPELARIA;
+-- CREATE DATABASE PAPELARIA;
 
 -----------------------------------------------------------
 --              ENTIDADES E RELACIONAMENTOS              --
 -----------------------------------------------------------
-DROP TABLE IF EXISTS CLIENTES;              -- entidade [X]
-DROP TABLE IF EXISTS CLIENTE_ADDRESS;       -- entidade [/]
-DROP TABLE IF EXISTS FUNCIONARIO;           -- entidade [X]
-DROP TABLE IF EXISTS ATENDENTE;             -- entidade [/]
-DROP TABLE IF EXISTS ENTREGADOR;            -- entidade [/]
-DROP TABLE IF EXISTS FUNCIONARIO_ADDRESS;   -- entidade [/]
-DROP TABLE IF EXISTS FORNECEDORES;          -- entidade [/]
-DROP TABLE IF EXISTS FORNECEDORES_ADDRESS;  -- entidade [/]
-DROP TABLE IF EXISTS PRODUTOS;              -- entidade [X]
-DROP TABLE IF EXISTS CATEGORIA;             -- entidade [X]
-DROP TABLE IF EXISTS COMPRAS;               -- entidade [X]
-DROP TABLE IF EXISTS ENTREGA;        -- relacionamentos [/]
-DROP TABLE IF EXISTS CARRINHO;       -- relacionamentos [/]
-DROP TABLE IF EXISTS GET_CATEGORIA;  -- relacionamentos [/]
-DROP TABLE IF EXISTS GET_PRODUTO;    -- relacionamentos [/]
+DROP TABLE IF EXISTS CLIENTES;                  -- entidade
+DROP TABLE IF EXISTS CLIENTE_ADDRESS;           -- entidade
+DROP TABLE IF EXISTS FUNCIONARIO;               -- entidade
+DROP TABLE IF EXISTS ATENDENTE;                 -- entidade
+DROP TABLE IF EXISTS ENTREGADOR;                -- entidade
+DROP TABLE IF EXISTS FUNCIONARIO_ADDRESS;       -- entidade
+DROP TABLE IF EXISTS FORNECEDORES;              -- entidade
+DROP TABLE IF EXISTS FORNECEDORES_ADDRESS;      -- entidade
+DROP TABLE IF EXISTS PRODUTOS;                  -- entidade
+DROP TABLE IF EXISTS CATEGORIA;                 -- entidade
+DROP TABLE IF EXISTS COMPRAS;                   -- entidade
+DROP TABLE IF EXISTS ENTREGA;            -- relacionamentos
+DROP TABLE IF EXISTS CARRINHO;           -- relacionamentos
+DROP TABLE IF EXISTS GET_PRODUTO;        -- relacionamentos
 -----------------------------------------------------------
 --                        TABELAS                        --
 -----------------------------------------------------------
-CREATE TABLE CLIENTES(
-    id INTEGER PRIMARY KEY,
-    cpf CHAR(11),
-    nome VARCHAR(50),
-    telefone1 CHAR(11),
-    telefone2 CHAR(11)
-);
-
-CREATE TABLE CLIENTE_ADDRESS(
-    CEP CHAR(8),
-    logradouro VARCHAR(50),
-    nro INTEGER,
-    UF CHAR(2),
-    cidade VARCHAR(50),
-    bairro VARCHAR(50),
-    pais VARCHAR(50),
-    complemento VARCHAR(50),
-    PRIMARY KEY(CEP, logradouro, nro)
-);
-
 CREATE TABLE FUNCIONARIO(
     id INTEGER PRIMARY KEY,
     cpf CHAR(11),
     nome VARCHAR(50),
     telefone1 CHAR(11),
     telefone2 CHAR(11)
-);
-
-CREATE TABLE ATENDENTE(
-    id_atendimento INTEGER
-);
-
-CREATE TABLE ENTREGADOR(
-    telefonePRO CHAR(11)
 );
 
 CREATE TABLE FUNCIONARIO_ADDRESS(
@@ -66,12 +37,87 @@ CREATE TABLE FUNCIONARIO_ADDRESS(
     bairro VARCHAR(50),
     pais VARCHAR(50),
     complemento VARCHAR(50),
-    PRIMARY KEY(CEP, logradouro, nro)
+    id_funcionario INTEGER,
+    FOREIGN KEY(id_funcionario) REFERENCES FUNCIONARIO(id) ON DELETE CASCADE,
+    PRIMARY KEY(CEP, logradouro, nro, id_funcionario)
+);
+
+CREATE TABLE ATENDENTE(
+    id_funcionario INTEGER,
+    FOREIGN KEY(id_funcionario) REFERENCES FUNCIONARIO(id),
+    PRIMARY KEY(id_funcionario)
+);
+
+CREATE TABLE ENTREGADOR(
+    id_funcionario INTEGER,
+    FOREIGN KEY(id_funcionario) REFERENCES FUNCIONARIO(id),
+    PRIMARY KEY(id_funcionario)
+);
+
+CREATE TABLE CLIENTES(
+    id INTEGER PRIMARY KEY,
+    cpf CHAR(11),
+    nome VARCHAR(50),
+    telefone1 CHAR(11),
+    telefone2 CHAR(11),
+    id_atendente INTEGER,
+    FOREIGN KEY(id_atendente) REFERENCES ATENDENTE(id_funcionario)
+);
+
+CREATE TABLE CLIENTE_ADDRESS(
+    CEP CHAR(8),
+    logradouro VARCHAR(50),
+    nro INTEGER,
+    UF CHAR(2),
+    cidade VARCHAR(50),
+    bairro VARCHAR(50),
+    pais VARCHAR(50),
+    complemento VARCHAR(50),
+    id_cliente INTEGER,
+    FOREIGN KEY(id_cliente) REFERENCES CLIENTES(id) ON DELETE CASCADE,
+    PRIMARY KEY(CEP, logradouro, nro, id_cliente)
+);
+
+CREATE TABLE CATEGORIA(
+    id INTEGER PRIMARY KEY,
+    nome VARCHAR(50),
+    descricao VARCHAR(500)
+);
+
+CREATE TABLE PRODUTOS(
+    codigo INTEGER PRIMARY KEY,
+    nome VARCHAR(50),
+    descricao VARCHAR(500),
+    qtde INTEGER,
+    secao VARCHAR(50),
+    id_categoria INTEGER,
+    FOREIGN KEY(id_categoria) REFERENCES CATEGORIA(id)
+);
+
+CREATE TABLE COMPRAS(
+    id_compra INTEGER PRIMARY KEY,
+    data_de_compra DATE,
+    na_loja BIT,
+    preco FLOAT,
+    id_cliente INTEGER,
+    FOREIGN KEY(id_cliente)
+);
+
+CREATE TABLE CARRINHO( -- compras N..N produtos
+    id_compra INTEGER,
+    id_produto INTEGER,
+    item VARCHAR(50),
+    qtde INTEGER,
+    preco FLOAT
+    data_de_compra DATE,
+    FOREIGN KEY(id_compra) REFERENCES COMPRAS(id_compra),
+    FOREIGN KEY(id_produto) REFERENCES PRODUTOS(codigo),
+    PRIMARY KEY(id_compra, id_produto)
 );
 
 CREATE TABLE FORNECEDORES(
     id INTEGER PRIMARY KEY,
-    cnpj CHAR(0), -- verificar
+    cnpj CHAR(14),
     nome VARCHAR(50),
     telefone1 CHAR(10),
     telefone2 char(11)
@@ -86,46 +132,24 @@ CREATE TABLE FORNECEDORES_ADDRESS(
     bairro VARCHAR(50),
     pais VARCHAR(50),
     complemento VARCHAR(50),
-    PRIMARY KEY(CEP, logradouro, nro)
+    id_fornecedores INTEGER,
+    FOREIGN KEY(id_fornecedores) REFERENCES FORNECEDORES(id) ON DELETE CASCADE,
+    PRIMARY KEY(CEP, logradouro, nro, id_fornecedores)
 );
 
-CREATE TABLE PRODUTOS(
-    codigo INTEGER PRIMARY KEY,
-    nome VARCHAR(50),
-    descricao VARCHAR(500),
-    qtde INTEGER,
-    secao VARCHAR(50)
-);
-
-CREATE TABLE CATEGORIA(
-    id INTEGER PRIMARY KEY,
-    nome VARCHAR(50),
-    descricao VARCHAR(500)
-);
-
-CREATE TABLE COMPRAS(
-    id_compra INTEGER PRIMARY KEY,
-    data_de_compra DATE,
-    preco FLOAT
-);
------------------------------------------------------------
-CREATE TABLE CARRINHO(
-    id_carrinho INTEGER PRIMARY KEY,
-    item VARCHAR(50),
-    qtde INTEGER,
-    preco FLOAT
-    data_de_compra DATE,
-);
-
-CREATE TABLE ENTREGA(
-    id_entrega INTEGER,
+CREATE TABLE ENTREGA( -- cliente N..N entregador
+    id_cliente INTEGER,
+    id_funcionario INTEGER,
     data_de_entrega DATE
+    FOREIGN KEY(id_cliente) REFERENCES CLIENTES(id),
+    FOREIGN KEY(id_funcionario) REFERENCES ENTREGADOR(id_funcionario),
+    PRIMARY KEY(id_cliente, id_funcionario)
 );
 
-CREATE TABLE GET_PRODUTO(
-
-);
-
-CREATE TABLE GET_CATEGORIA(
-
+CREATE TABLE GET_PRODUTO( -- produtos N..N fornecedores
+    id_fornecedores INTEGER,
+    id_produto INTEGER,
+    FOREIGN KEY(id_fornecedores) REFERENCES FORNECEDORES(id),
+    FOREIGN KEY(id_produto) REFERENCES PRODUTOS(codigo),
+    PRIMARY KEY(id_fornecedores, id_produto)
 );
