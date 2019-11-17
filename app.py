7f2b88b2fbe581b71d1ca6de510dfa1ac86e9f9a
giorgiobraz <giorgiobraz@gmail.com>
2019-11-17 18:43:40 -0300

    método "adicionar" criado e funcional estaticamente

--- app.py
+++ app.py
@@ -4,22 +4,42 @@ from flask import render_template, url_for, request, redirect
 
 app = Flask(__name__)
 
+##############################################################################
+#                          CRUD SIMPLES >> CATEGORIAS                        #
+##############################################################################
 class Categorias:
     def __init__(self, id, nome, descricao):
         self.id = id
         self.nome = nome
         self.descricao = descricao
 
-##############################################################################
-#                             ROTAS PARA CATEGORIA                           #
-##############################################################################
+cat1 = Categorias(123, 'Artes', 'randon text1')
+cat2 = Categorias(213, 'Games', 'randon text2')
+lista = [cat1,cat2]
+
+# --------------------------------- EXIBIR --------------------------------- #
 @app.route('/categoria')
-def categoria():
-    cat1 = Categorias(123, 'Artes', 'randon text1')
-    cat2 = Categorias(213, 'Games', 'randon text2')
-    cat3 = Categorias(321, 'Filmes', 'randon text3')
-    lista = [cat1,cat2,cat3]
+def exibir():
     return render_template('homepage.html', titulo='Categorias', Categorias=lista)
 
+# ------------------------------- ADICIONAR -------------------------------- #
+@app.route('/categoria', methods=['POST',])
+def adicionar():
+    id = request.form['id']
+    nome = request.form['nome']
+    descricao = request.form['descricao']
+    cat3 = Categorias(id, nome,descricao)
+    lista.append(cat3)
+    return render_template('homepage.html', titulo='Categorias', Categorias=lista)
+
+# --------------------------------- EDITAR --------------------------------- #
+@app.route('/categoria')
+def editar():
+    pass
+
+# --------------------------------- DELETAR -------------------------------- #
+@app.route('/categoria')
+def deletar():
+    pass
 
 app.run(debug=True)
