b3e8f5921dce80b31ead9dc64214ffa016ac7a0b
Giorgio Braz <giorgiobraz@gmail.com>
2019-12-06 14:53:55 -0300

    criação dos métodos para PRODUTOS

--- app.py
+++ app.py
@@ -5,34 +5,82 @@ from connectDatabase import Categorias
 app = Flask(__name__)
 sql = Categorias(None, None)
 
-##############################################################################
-#                          CRUD SIMPLES >> CATEGORIAS                        #
-##############################################################################
+###################################################################################
+# 								    CRUD CATEGORIAS
+###################################################################################
 
-# --------------------------------- EXIBIR --------------------------------- #
+# Exibir categoria
 @app.route('/categoria')
 def exibir():
     results = sql.get_all_categoria()
-    print(results)
     return render_template('homepage.html', titulo='Categorias', Categorias=results)
 
-# ------------------------------- ADICIONAR -------------------------------- #
+# Adicionar categoria
 @app.route('/categoria', methods=['POST',])
 def adicionar():
-    id = request.form['id']
     nome = request.form['nome']
     descricao = request.form['descricao']
-    nova_categoria = sql.nova_categoria(nome, descricao)
+    add = sql.nova_categoria(nome, descricao)
     results = sql.get_all_categoria()
     return render_template('homepage.html', titulo='Categorias', Categorias=results)
 
-# --------------------------------- EDITAR --------------------------------- #
-@app.route('/categoria')
+# Editar categoria
+@app.route('/categoria', methods=['POST',])
 def editar():
+    id = request.args.get('id')
+    nome = request.form['nome']
+    descricao = request.form['descricao']
+    update = sql.edit_nome_categoria(id, nome, descricao)
+    results = sql.get_all_categoria()
+    return render_template('homepage.html', titulo='Categorias', Categorias=results)
+
+
+# Deletar categoria
+@app.route('/categoria')
+def deletar():
     pass
 
-# --------------------------------- DELETAR -------------------------------- #
-@app.route('/categoria')
+
+###################################################################################
+# 								    CRUD PRODUTOS
+###################################################################################
+
+# Exibir produtos
+@app.route('/produto')
+def exibir():
+    results = sql.get_all_produtos()
+    return render_template('produtos.html', titulo='Produtos', Produtos=results)
+
+
+# Adicionar produto
+@app.route('/produto', methods=['POST',])
+def adicionar():
+    nome = request.form['nome']
+    descricao = request.form['descricao']
+    qtde = request.form['qtde']
+    secao = request.form['secao']
+    id_categoria = request.form['id_categoria']
+    add = sql.novo_produto(nome, descricao, qtde, secao, id_categoria)
+    results = sql.get_all_produtos()
+    return render_template('produtos.html', titulo='Produtos', Produtos=results)
+
+
+# Editar produto
+@app.route('/produto', methods=['POST',])
+def editar():
+    id = request.args.get('id')
+    nome = request.form['nome']
+    descricao = request.form['descricao']
+    qtde = request.form['qtde']
+    secao = request.form['secao']
+    id_categoria = request.form['id_categoria']
+    update = sql.edit_nome_produto(id, nome, descricao)
+    results = sql.get_all_produtos()
+    return render_template('produtos.html', titulo='Produtos', Produtos=results)
+
+
+# Deletar produto
+@app.route('/produto')
 def deletar():
     pass
 
